cd59548da9ab89bd2bc713e7c3a73f90dcb8ae19
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-06-15 21:14:56 +0300

    first api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 const app = express()
 app.use(cors)
 app.get('/',(req,res)=>{
-    res.send("hello thus is my first api")
+    res.send("hello this is my first api")
 
 })
 app.listen(3000,"Server is running on port 3000!")
